1f2b86a93ec35eab35087e9688ac77abcf5f07b1
JMVillota <jeffersonvillota@gmail.com>
2022-08-18 03:18:20 -0500

    Nuevos Cambios

---
+++ calendario.php
@@ -0,0 +1,118 @@
+<!doctype html>
+<html lang="es-ES">
+
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="wp-content/themes/utndigital/assets/img/UTN.png" type="image/png" sizes="16x16">
+
+    <meta name='robots' content='index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1' />
+
+    <title>Home - UTN Digital</title>
+    <link rel="canonical" href="index.html" />
+
+    <link rel='dns-prefetch' href='http://s.w.org/' />
+    <link rel="alternate" type="application/rss+xml" title="UTNDigital &raquo; Feed" href="feed/index.html" />
+    <link rel="alternate" type="application/rss+xml" title="UTNDigital &raquo; Comments Feed" href="comments/feed/index.html" />
+    <link rel='stylesheet' id='wp-block-library-css' href='wp-includes/css/dist/block-library/style.minf049.css?ver=6.0' media='all' />
+
+    <link rel='stylesheet' id='utndigital-style-css' href='wp-content/themes/utndigital/style8a54.css?ver=1.0.0' media='all' />
+    <link rel='stylesheet' id='bootstrap-css-css' href='wp-content/themes/utndigital/assets/css/bootstrap.min0a4b.css?v=1655217998&amp;ver=6.0' media='all' />
+    <link rel='stylesheet' id='owl-css-css' href='wp-content/themes/utndigital/assets/css/owl.carousel.min0a4b.css?v=1655217998&amp;ver=6.0' media='all' />
+    <link rel='stylesheet' id='owl-default-css' href='wp-content/themes/utndigital/assets/css/owl.theme.default.min0a4b.css?v=1655217998&amp;ver=6.0' media='all' />
+    <link rel='stylesheet' id='locomotive-css-css' href='wp-content/themes/utndigital/assets/css/locomotive-scroll0a4b.css?v=1655217998&amp;ver=6.0' media='all' />
+    <link rel='stylesheet' id='style-css-css' href='wp-content/themes/utndigital/assets/css/style.min0a4b.css' media='all' />
+    <link rel="https://api.w.org/" href="wp-js/index.html" />
+    <link rel="alternate" type="application/js" href="wp-js/wp/v2/pages/7.html" />
+    <link rel="EditURI" type="application/rsd+xml" title="RSD" href="xmlrpc0db0.html?rsd" />
+    <meta name="generator" content="WordPress 6.0" />
+    <link rel='shortlink' href='index.html' />
+    <style>
+        .recentcomments a {
+            display: inline !important;
+            padding: 0 !important;
+            margin: 0 !important;
+        }
+    </style>
+</head>
+<div class="preloader">
+    <div class="text-wrapper">
+        <div class="text-animation">
+            <div class="loader-font text page-name page-About"><span class="dot"></span> <span class="title-wrap"> <span class="title-span">Calendario</span> </span>
+            </div>
+        </div>
+    </div>
+</div>
+
+<body id="js-scroll " class="page-template-default page page-id-59 o-scroll" data-scroll-container>
+    <div id="page" class="site">
+        <div class="main-logo">
+            <a href="index.html">
+                <img src="wp-content/themes/utndigital/assets/img/UTN.png" alt="Inicio" />
+            </a>
+        </div>
+        <div class="main-menu">
+            <div class="main-menu-icon">
+                <span></span>
+                <span></span>
+            </div>
+        </div>
+        <div class="main-menu-wrapper">
+            <div class="main-menu-content">
+                <div class="menu-menu-1-container">
+                    <ul id="primary-menu" class="menu">
+                        <li id="menu-item-146" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-home current-menu-item page_item page-item-7 current_page_item menu-item-146">
+                            <a href="index.html" aria-current="page">Inicio<img class="image" src=wp-content/uploads/2022/04/inicio.png>
+                            </a>
+                        </li>
+                        <li id="menu-item-193" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-193"><a href="calendario.php">Calendario<img class="image" src=wp-content/uploads/2022/04/calendario.png></a></li>
+                        <li id="menu-item-671" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-671"><a href="index.html">Información<img class="image" src=wp-content/uploads/2022/03/contacto.png></a></li>
+                        <li id="menu-item-147" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-147"><a href="login.php">Login<img class="image" src=wp-content/uploads/2022/03/consultas.png></a></li>
+                    </ul>
+                </div>
+                <div class="social-menu-wrapper">
+                    <div class="menu-social-menu-container">
+                        <ul id="social-menu" class="menu">
+                            <li id="menu-item-152" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-152"><a href="https://www.linkedin.com/school/universidad-t%C3%A9cnica-del-norte/">Linkedin</a>
+                            </li>
+                            <li id="menu-item-151" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-151"><a href="https://www.instagram.com/utn_ec/">Instagram</a></li>
+                            <li id="menu-item-149" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-149"><a href="https://www.facebook.com/utnibarra.ec">Facebook</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- #colophon -->
+    </div>
+    </div>
+    <script src='wp-content/themes/utndigital/js/navigation8a54.js?ver=1.0.0' id='pulladigital-navigation-js'></script>
+    <script src='wp-content/themes/utndigital/assets/js/jquery.min8a54.js?ver=1.0.0' id='pulladigital-jquery-js'></script>
+    <script src='wp-content/themes/utndigital/assets/js/TweenMax.min8a54.js?ver=1.0.0' id='pulladigital-TweenMax-js'></script>
+    <script src='wp-content/themes/utndigital/assets/js/owl.carousel.min8a54.js?ver=1.0.0' id='pulladigital-owl-carousel-js'></script>
+    <script src='wp-content/themes/utndigital/assets/js/locomotive-scroll.min8a54.js?ver=1.0.0' id='locomotive-scroll-js'></script>
+    <script src='wp-content/themes/utndigital/assets/js/swiper-bundle.min8a54.js?ver=1.0.0' id='swiper-js-js'></script>
+    <script id='pulladigital-main-js-extra'>
+        var javascript_object = {
+            "assets_url ": "http:\/\/pulla.digital\/wp-content\/themes\/pulladigital\/assets\/js\/ ",
+            "is_blog ": " ",
+            "is_single ": "1 "
+        };
+    </script>
+    <script src='wp-content/themes/utndigital/assets/js/main8a54.js?ver=1.0.0' id='pulladigital-main-js'></script>
+</body>
+
+</html>
+<style>
+    html {
+        margin-top: 0px !important;
+    }
+
+    .site-footer {
+        position: relative;
+        background: #fff;
+        z-index: 9;
+    }
+</style>
+</div>
--- login.php
+++ login.php
@@ -57,12 +57,13 @@ if (isset($_POST['login'])) {
 
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
 <head>
   <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie-edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" href="wp-content/themes/utndigital/assets/img/UTN.png" type="image/png" sizes="16x16">
+  <meta name='robots' content='index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1' />
   <title>Login</title>
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap" rel="stylesheet">
   <link rel="stylesheet" type="text/css " href="assets/css/style.css">
@@ -70,9 +71,18 @@ if (isset($_POST['login'])) {
   <link rel="stylesheet" href="assets/css/sweetalert.css">
   <link rel="icon" href="wp-content/themes/utndigital/assets/img/UTN.png" type="image/x-icon" />
   <script src="https://kit.fontawesome.com/a81368914c.js"></script>
+  <link rel='stylesheet' id='style-css-css' href='wp-content/themes/utndigital/assets/css/style.min0a4b.css' media='all' />
 </head>
+<div class="preloader">
+  <div class="text-wrapper">
+    <div class="text-animation">
+      <div class="loader-font text page-name page-About"><span class="dot"></span> <span class="title-wrap"> <span class="title-span">Login</span> </span>
+      </div>
+    </div>
+  </div>
+</div>
 
-<body>
+<body  id="js-scroll " class="page-template-default page page-id-59 o-scroll" data-scroll-container>
   <style>
     div.a {
       text-align: center;
@@ -81,9 +91,40 @@ if (isset($_POST['login'])) {
       justify-content: center;
     }
   </style>
+  <div class="main-menu">
+    <div class="main-menu-icon">
+      <span></span>
+      <span></span>
+    </div>
+  </div>
+  <div class="main-menu-wrapper">
+    <div class="main-menu-content">
+      <div class="menu-menu-1-container">
+        <ul id="primary-menu" class="menu">
+          <li id="menu-item-146" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-home current-menu-item page_item page-item-7 current_page_item menu-item-146">
+            <a href="index.html" aria-current="page">Inicio<img class="image" src=wp-content/uploads/2022/04/inicio.png>
+            </a>
+          </li>
+          <li id="menu-item-193" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-193"><a href="calendario.php">Calendario<img class="image" src=wp-content/uploads/2022/04/calendario.png></a></li>
+          <li id="menu-item-671" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-671"><a href="index.html">Información<img class="image" src=wp-content/uploads/2022/03/contacto.png></a></li>
+          <li id="menu-item-147" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-147"><a href="login.php">Login<img class="image" src=wp-content/uploads/2022/03/consultas.png></a></li>
+        </ul>
+      </div>
+      <div class="social-menu-wrapper">
+        <div class="menu-social-menu-container">
+          <ul id="social-menu" class="menu">
+            <li id="menu-item-152" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-152"><a href="https://www.linkedin.com/school/universidad-t%C3%A9cnica-del-norte/">Linkedin</a>
+            </li>
+            <li id="menu-item-151" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-151"><a href="https://www.instagram.com/utn_ec/">Instagram</a></li>
+            <li id="menu-item-149" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-149"><a href="https://www.facebook.com/utnibarra.ec">Facebook</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
   <div class="contenedor">
     <div class="img">
-      <img src="https://pngimage.net/wp-content/uploads/2018/05/calendrier-icone-png-4.png" alt="">
+      <img src="assets/img/logo_Fica.jpg" alt="">
     </div>
     <div class="contenido-login">
 
@@ -125,7 +166,7 @@ if (isset($_POST['login'])) {
         </div>
 
 
-        <button class="btn btn-success" name='login' type="success"> Iniciar sesion
+        <button class="btn btn-primary" name='login' type="primary"> Iniciar sesion
         </button>
 
       </form>
@@ -133,10 +174,19 @@ if (isset($_POST['login'])) {
 
     </div>
   </div>
+
   <script src="assets/js/jquery.js"></script>
   <script src="assets/js/sweetalert.min.js"></script>
   <!-- Js personalizado -->
 
+
+
+  <script src='wp-content/themes/utndigital/assets/js/owl.carousel.min8a54.js' id='pulladigital-owl-carousel-js'></script>
+
+  <script src='wp-content/themes/utndigital/assets/js/locomotive-scroll.min8a54.js?ver=1.0.0' id='locomotive-scroll-js'></script>
+  <script src='wp-content/themes/utndigital/assets/js/swiper-bundle.min8a54.js?ver=1.0.0' id='swiper-js-js'></script>
+
+  <script src='wp-content/themes/utndigital/assets/js/main8a54.js?ver=1.0.0' id='pulladigital-main-js'></script>
 
 </body>
 
