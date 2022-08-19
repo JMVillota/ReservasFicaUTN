16691ba933948fa501198ba410731d2a52462694
JMVillota <jeffersonvillota@gmail.com>
2022-08-19 03:49:48 -0500

    Calendario

---
+++ assets/js/script.js
@@ -0,0 +1,56 @@
+var calendar;
+var Calendar = FullCalendar.Calendar;
+var events = [];
+$(function() {
+    if (!!scheds) {
+        Object.keys(scheds).map(k => {
+            var row = scheds[k]
+            events.push({ id: row.reservacion_id, title: row.descripcion_evento, start: row.fecha_inicio, end: row.fecha_fin });
+        })
+    }
+    var date = new Date()
+    var d = date.getDate(),
+        m = date.getMonth(),
+        y = date.getFullYear()
+
+    calendar = new Calendar(document.getElementById('calendar'), {
+        locale: 'es',
+        headerToolbar: {
+            left: 'prev,next today',
+            right: 'dayGridMonth,dayGridWeek,list',
+            center: 'title',
+        },
+        selectable: true,
+        themeSystem: 'bootstrap',
+        //Random default events
+        events: events,
+        eventClick: function(info) {
+
+            var _details = $('#event-details-modal')
+            var id = info.event.id
+            if (!!scheds[id]) {
+
+                _details.find('#fecha_inicio').text(scheds[id].sdate)
+                _details.find('#fecha_fin').text(scheds[id].edate)
+                _details.find('#nombre_evento').text(scheds[id].nombre_evento)
+                _details.find('#descripcion_evento').text(scheds[id].descripcion_evento)
+                _details.find('#nombre_departamento').text(scheds[id].nombre_departamento)
+                _details.find('#apellidos_responsable').text(scheds[id].apellidos_responsable)
+                _details.find('#nombres_responsable').text(scheds[id].nombres_responsable)
+                _details.find('#estado_reservacion').text(scheds[id].estado_reservacion)
+                _details.find('#estado_evento').text(scheds[id].estado_evento)
+                _details.find('#nombre_lugar').text(scheds[id].nombre_lugar)
+                    // _details.find('#edit,#delete').attr('data-id', id)
+                _details.modal('show')
+            } else {
+                alert("Event is undefined");
+            }
+        },
+        eventDidMount: function(info) {
+            // Do Something after events mounted
+        },
+        editable: false
+    });
+
+    calendar.render();
+})
--- calendario.php
+++ calendario.php
@@ -1,135 +1,135 @@
-<!doctype html>
-<html lang="es-ES">
-
-<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<?php require_once('assets/db/db-connect.php') ?>
+<!DOCTYPE html>
+<html lang="es">
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" href="wp-content/themes/utndigital/assets/img/UTN.png" type="image/png" sizes="16x16">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calendario FICA</title>
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+    <link rel="stylesheet" href="wp-content/themes/utndigital/assets/css/bootstrap.min0a4b.css">
+    <link rel="stylesheet" href="/assets/fullcalendar/lib/main.css">
+    <script src="/assets/js/jquery-3.6.0.min.js"></script>
+    <script src="/assets/js/bootstrap.min.js"></script>
+    <script src="/assets/fullcalendar/lib/main.min.js"></script>
+    <script src="/assets/fullcalendar/lib/locales-all.js"></script>
+    <style>
+        :root {
+            --bs-success-rgb: 71, 222, 152 !important;
+        }
 
-    <meta name='robots' content='index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1' />
+        html,
+        body {
+            height: 100%;
+            width: 100%;
+            font-family: Arial, Helvetica, sans-serif, cursive;
+        }
 
-    <title>Home - UTN Digital</title>
-    <link rel="canonical" href="index.html" />
+        .btn-info.text-light:hover,
+        .btn-info.text-light:focus {
+            background: #000;
+        }
 
-    <link rel='dns-prefetch' href='http://s.w.org/' />
-    <link rel="alternate" type="application/rss+xml" title="UTNDigital &raquo; Feed" href="feed/index.html" />
-    <link rel="alternate" type="application/rss+xml" title="UTNDigital &raquo; Comments Feed" href="comments/feed/index.html" />
-    <link rel='stylesheet' id='wp-block-library-css' href='wp-includes/css/dist/block-library/style.minf049.css?ver=6.0' media='all' />
-
-    <link rel='stylesheet' id='utndigital-style-css' href='wp-content/themes/utndigital/style8a54.css?ver=1.0.0' media='all' />
-    <link rel='stylesheet' id='bootstrap-css-css' href='wp-content/themes/utndigital/assets/css/bootstrap.min0a4b.css?v=1655217998&amp;ver=6.0' media='all' />
-    <link rel='stylesheet' id='owl-css-css' href='wp-content/themes/utndigital/assets/css/owl.carousel.min0a4b.css?v=1655217998&amp;ver=6.0' media='all' />
-    <link rel='stylesheet' id='owl-default-css' href='wp-content/themes/utndigital/assets/css/owl.theme.default.min0a4b.css?v=1655217998&amp;ver=6.0' media='all' />
-    <link rel='stylesheet' id='locomotive-css-css' href='wp-content/themes/utndigital/assets/css/locomotive-scroll0a4b.css?v=1655217998&amp;ver=6.0' media='all' />
-    <link rel='stylesheet' id='style-css-css' href='wp-content/themes/utndigital/assets/css/style.min0a4b.css' media='all' />
-    <link rel="https://api.w.org/" href="wp-js/index.html" />
-    <link rel="alternate" type="application/js" href="wp-js/wp/v2/pages/7.html" />
-    <link rel="EditURI" type="application/rsd+xml" title="RSD" href="xmlrpc0db0.html?rsd" />
-    <meta name="generator" content="WordPress 6.0" />
-    <link rel='shortlink' href='index.html' />
-    <link href='assets/css/main.css' rel='stylesheet' />
-    <script src='assets/js/main.js'></script>
-    <script src='assets/js/locales-all.js'></script>
-    <script src='assets/js/sweetalert2.all.min.js'></script>
+        table,
+        tbody,
+        td,
+        tfoot,
+        th,
+        thead,
+        tr {
+            border-color: #ededed !important;
+            border-style: solid;
+            border-width: 1px !important;
+        }
+    </style>
 </head>
-<div class="preloader">
-    <div class="text-wrapper">
-        <div class="text-animation">
-            <div class="loader-font text page-name page-About"><span class="dot"></span> <span class="title-wrap"> <span class="title-span">Calendario</span> </span>
+
+<body class="bg-light">
+    <div class="container py-5" id="page-container">
+        <div class="row">
+            <div class="col-md-9">
+                <div id="calendar"></div>
             </div>
+ 
         </div>
     </div>
-</div>
-
-<body id="js-scroll " class="page-template-default page page-id-59 o-scroll" data-scroll-container>
-    
-    <div id="page" class="site">
-        <div class="main-logo">
-            <a href="index.html">
-                <img src="wp-content/themes/utndigital/assets/img/UTN.png" alt="Inicio" />
-            </a>
-        </div>
-        <div class="main-menu">
-            <div class="main-menu-icon">
-                <span></span>
-                <span></span>
-            </div>
-        </div>
-        <div class="main-menu-wrapper">
-            <div class="main-menu-content">
-                <div class="menu-menu-1-container">
-                    <ul id="primary-menu" class="menu">
-                        <li id="menu-item-146" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-home current-menu-item page_item page-item-7 current_page_item menu-item-146">
-                            <a href="index.html" aria-current="page">Inicio<img class="image" src=wp-content/uploads/2022/04/inicio.png>
-                            </a>
-                        </li>
-                        <li id="menu-item-193" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-193"><a href="calendario.php">Calendario<img class="image" src=wp-content/uploads/2022/04/calendario.png></a></li>
-                        <li id="menu-item-671" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-671"><a href="index.html">Información<img class="image" src=wp-content/uploads/2022/03/contacto.png></a></li>
-                        <li id="menu-item-147" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-147"><a href="login.php">Login<img class="image" src=wp-content/uploads/2022/03/consultas.png></a></li>
-                    </ul>
+    <!-- Event Details Modal -->
+    <div class="modal fade" tabindex="-1" data-bs-backdrop="static" id="event-details-modal">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content rounded-0">
+                <div class="modal-header rounded-0">
+                    <h5 class="modal-title">Detalles de la Reservación</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-                <div class="social-menu-wrapper">
-                    <div class="menu-social-menu-container">
-                        <ul id="social-menu" class="menu">
-                            <li id="menu-item-152" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-152"><a href="https://www.linkedin.com/school/universidad-t%C3%A9cnica-del-norte/">Linkedin</a>
-                            </li>
-                            <li id="menu-item-151" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-151"><a href="https://www.instagram.com/utn_ec/">Instagram</a></li>
-                            <li id="menu-item-149" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-149"><a href="https://www.facebook.com/utnibarra.ec">Facebook</a></li>
-                        </ul>
+                <div class="modal-body rounded-0">
+                    <div class="container-fluid">
+                        <dl>
+
+                            <dt class="text-muted">Inicio</dt>
+                            <dd id="fecha_inicio" class=""></dd>
+
+                            <dt class="text-muted">Fin</dt>
+                            <dd id="fecha_fin" class=""></dd>
+
+                            <dt class="text-muted">Tipo de Evento</dt>
+                            <dd id="nombre_evento" class=""></dd>
+
+                            <dt class="text-muted">Descripción</dt>
+                            <dd id="descripcion_evento" class=""></dd>
+
+                            <dt class="text-muted">Carrera</dt>
+                            <dd id="nombre_departamento" class=""></dd>
+
+                            <dt class="text-muted">Apellidos Responsable</dt>
+                            <dd id="apellidos_responsable" class=""></dd>
+
+                            <dt class="text-muted">Nombres Responsable</dt>
+                            <dd id="nombres_responsable" class=""></dd>
+
+                            <dt class="text-muted">Estado Reservación</dt>
+                            <dd id="estado_reservacion"></dd>
+
+                            <dt class="text-muted">Estado Evento</dt>
+                            <dd id="estado_evento"></dd>
+
+                            <dt class="text-muted">Lugar Evento</dt>
+                            <dd id="nombre_lugar" class=""></dd>
+                        </dl>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="section about-parallax" data-scroll-section>
-                    <div class="container-wrapper container-width-4 vartical-padding-1 two-side-padding-mobile vartical-padding-top-mobile-3">
-                        <div class="container-fluid gx-0">
-                            <div class="row gx-0">
+    </div>
+    <!-- Event Details Modal -->
 
-                                <div class="col-12 m-only">
-                                <div id='calendar'></div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-    </div>
-    </div>
-    <script>
-        document.addEventListener('DOMContentLoaded', function() {
-            var calendarEl = document.getElementById('calendar');
-            var calendar = new FullCalendar.Calendar(calendarEl, {
-                initialView: 'dayGridMonth',
-                locale:"es",
-                headerToolbar:{
-                    left:'prev,next today',
-                    center:'title',
-                    right:'dayGridMonth,timeGridWeek,timeGridDay'
-                }
-            });
-            calendar.render();
-        });
-    </script>
-    <script src='wp-content/themes/utndigital/js/navigation8a54.js?ver=1.0.0' id='pulladigital-navigation-js'></script>
-    <script src='wp-content/themes/utndigital/assets/js/jquery.min8a54.js?ver=1.0.0' id='pulladigital-jquery-js'></script>
-    <script src='wp-content/themes/utndigital/assets/js/TweenMax.min8a54.js?ver=1.0.0' id='pulladigital-TweenMax-js'></script>
-    <script src='wp-content/themes/utndigital/assets/js/owl.carousel.min8a54.js?ver=1.0.0' id='pulladigital-owl-carousel-js'></script>
-    <script src='wp-content/themes/utndigital/assets/js/locomotive-scroll.min8a54.js?ver=1.0.0' id='locomotive-scroll-js'></script>
-    <script src='wp-content/themes/utndigital/assets/js/swiper-bundle.min8a54.js?ver=1.0.0' id='swiper-js-js'></script>
-    <script src='wp-content/themes/utndigital/assets/js/main8a54.js?ver=1.0.0' id='pulladigital-main-js'></script>
+    <?php
+    $schedules = $conn->query("SELECT dr.*, d.nombre_departamento, le.nombre_lugar, e.nombre_evento FROM detalle_reservaciones dr INNER JOIN departamentos d ON dr.departamento_id = d.departamento_id INNER JOIN eventos e ON dr.tipo_evento_id = e.evento_id INNER JOIN lugar_eventos le ON dr.lugar_evento_id = le.lugar_evento_id");
+    $sched_res = [];
+    foreach ($schedules->fetch_all(MYSQLI_ASSOC) as $row) {
+        locale: 'es';
+        $row['sdate'] = date("M d, Y h:i A", strtotime($row['fecha_inicio']));
+        $row['edate'] = date("M d, Y h:i A", strtotime($row['fecha_fin']));
+        if ($row['estado_reservacion'] == 1) {
+            $row['estado_reservacion'] = 'Aceptado';
+        } else {
+            $row['estado_reservacion'] = 'Pendiente';
+        }
+        if ($row['estado_evento'] == 1) {
+            $row['estado_evento'] = 'Realizado';
+        } else {
+            $row['estado_evento'] = 'No Realizado';
+        }
+        $sched_res[$row['reservacion_id']] = $row;
+    }
+    ?>
+    <?php
+    if (isset($conn)) $conn->close();
+    ?>
 </body>
+<script>
+    var scheds = $.parseJSON('<?= json_encode($sched_res) ?>')
+</script>
+<script src="/assets/js/script.js"></script>
 
 </html>
-<style>
-    html {
-        margin-top: 0px !important;
-    }
-
-    .site-footer {
-        position: relative;
-        background: #fff;
-        z-index: 9;
-    }
-</style>
-</div>
